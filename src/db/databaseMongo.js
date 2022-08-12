08dbe8e1244474b8f840fd7e8ba1d934805c69ed
Ornela Anahi Lugo Aquino <nelladelugo2@gmail.com>
2022-08-11 21:35:17 -0300

    basededatos

--- src/db/databaseMongo.js
+++ src/db/databaseMongo.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-mongoose.connect("mongodb+srv://user_coder:******@cluster0.b8idn.mongodb.net/usuarioMongo?retryWrites=true&w=majority").then(() => {
+mongoose.connect("mongodb+srv://user_coder:42856227@cluster0.b8idn.mongodb.net/usuarioMongo?retryWrites=true&w=majority").then(() => {
   console.log("Conexion a MongoDB correcta")
 }).catch(err => {
   console.log(err)
--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ if (MODO === 'FORK') {
       secure: false,
       maxAge: 600000
     },
-    store: MongoStore.create({ mongoUrl: "mongodb+srv://user_coder:*****@cluster0.b8idn.mongodb.net/sessionMongo?retryWrites=true&w=majority" })
+    store: MongoStore.create({ mongoUrl: "mongodb+srv://user_coder:42856227@cluster0.b8idn.mongodb.net/sessionMongo?retryWrites=true&w=majority" })
   }))
 
   app.use(passport.initialize())
@@ -94,7 +94,7 @@ else if (MODO === 'CLUSTER') {
         secure: false,
         maxAge: 600000
       },
-      store: MongoStore.create({ mongoUrl: "mongodb+srv://user_coder:*******@cluster0.b8idn.mongodb.net/sessionMongo?retryWrites=true&w=majority" })
+      store: MongoStore.create({ mongoUrl: "mongodb+srv://user_coder:42856227@cluster0.b8idn.mongodb.net/sessionMongo?retryWrites=true&w=majority" })
     }))
 
     app.use(passport.initialize())
